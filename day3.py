5ba09d9033550dc669114834f294ff1396d98a4f
Xinepeng <85253873+poopyhed444@users.noreply.github.com>
2024-12-04 14:05:14 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,19 +1,38 @@
+# import re
+
+
+# search_string = ""
+# for i in range(6):
+#     search_string += input()
+# regex = "mul\(([1-9]*),([1-9]*)\)"
+
+# matches = re.findall(regex, search_string)
+
+# print(matches)
+# sums =0
+# for match in matches:
+#     numbers =[]
+#     for num in match:
+#         numbers.append(int(num))
+#     sums += (numbers[0] * numbers[1])
+# print(sums)
+
+
+
 import re
 
 
 search_string = ""
-for i in range(6):
+for i in range(1):
     search_string += input()
-regex = "mul\(([1-9]*),([1-9]*)\)"
-
-matches = re.findall(regex, search_string)
-
-print(matches)
-sums =0
+regex = "mul\(([1-9]*),([1-9]*)\)|do(())|don'\t(())"
+matches = re.finditer(regex, search_string)
+total =0
+# for match in matches:
+#     print(match.group())
+flag = False
 for match in matches:
-    numbers =[]
-    for num in match:
-        numbers.append(int(num))
-    sums += (numbers[0] * numbers[1])
-print(sums)
-
+    if "mul" in match.group() and flag == False:
+        sigma = match.group().split(",")
+        print(sigma)
+        
