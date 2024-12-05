1ce9c20fa7877d5812f241d067c55afeb8e13d5b
Xinepeng <85253873+poopyhed444@users.noreply.github.com>
2024-12-05 14:04:40 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -23,16 +23,26 @@ import re
 
 
 search_string = ""
-for i in range(1):
+for i in range(6):
     search_string += input()
-regex = "mul\(([1-9]*),([1-9]*)\)|do(())|don'\t(())"
+regex = r"mul\((\d*),(\d*)\)|do\(\)|don't\(\)"
 matches = re.finditer(regex, search_string)
 total =0
-# for match in matches:
-#     print(match.group())
 flag = False
 for match in matches:
+    if "don't()" in match.group():
+        flag = True
+    if "do()" in match.group():
+        flag = False
     if "mul" in match.group() and flag == False:
-        sigma = match.group().split(",")
-        print(sigma)
-        
+        sigma = match.group()
+        # print(sigma)
+        pattern = re.compile("(\d*),(\d*)")
+        sus = pattern.findall(sigma)
+        for i in sus:
+            num1 = int(i[0])
+            num2 = int(i[1])
+            total += (num1 * num2)
+            
+
+print(total)
