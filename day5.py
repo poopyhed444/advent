be674d07a2b34d094520dda4a0afc1d25b9bfbb3
Xinepeng <85253873+poopyhed444@users.noreply.github.com>
2024-12-11 13:33:39 -0500

    Update day5.py

--- day5.py
+++ day5.py
@@ -51,7 +51,20 @@ def swap(skibidi):
 	skibidi[0] = skibidi[1]
 	skibidi[1] = a
 	return skibidi
+
+def keep_swapping(nums, testlist):
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(nums) - 1):
+            a = [nums[i], nums[i+1]]
+            if a not in testlist and swap(a) in testlist:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                changed = True
+    return nums
+
 sigma = []
+
 
 while True:
     try:
@@ -67,36 +80,23 @@ mids = []
 try: 
     while True:
         inputs = input()
-        testlist = []
-        if "," in inputs:
-            nums = [int(x) for x in inputs.split(",")]
-        else: 
+        if "," not in inputs:
             break
+        nums = [int(x) for x in inputs.split(",")]
+        testlist = []
         for i in range(len(nums)):
             for j in range(len(sigma)):
                 if (nums[i] == sigma[j][0]):
                     testlist.append(sigma[j])
-        flag = False
-        for i in range(len(nums) - 1):
-            a = []
-            a.append(nums[i])
-            a.append(nums[i+1])
-            if (a in testlist):
-                continue
-            else:
-                flag = True
-                skibidi = swap(a)
-                if (skibidi in testlist):
-                    print(nums)
-                    nums[i], nums[i+1] = nums[i+1], nums[i]
-                    print(nums)
-        if (flag == True):
-            print(nums[int((len(nums) - 1) / 2)])
-            mids.append(nums[int((len(nums) - 1) / 2)])
+        original_nums = nums.copy()
+        nums = keep_swapping(nums, testlist)
+        
+        if nums != original_nums:
+            mid = nums[len(nums) // 2]
+            mids.append(mid)
 
 except EOFError:
     pass
-
 
 
 print(sum(mids))
