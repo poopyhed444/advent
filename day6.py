c616a28734b15452aab209318d82a2f7e085dfab
Xinepeng <85253873+poopyhed444@users.noreply.github.com>
2024-12-16 14:05:24 -0500

    Update day6.py

--- day6.py
+++ day6.py
@@ -17,16 +17,16 @@ def switchto(pointer):
         return "<"
     if (pointer == "<"):
         return "^"
-while True:
-    try:
-        line = input()
+
+            
+        
+with open('input_file.txt', 'r') as file:
+    sigma = []
+    for line in file:
+        line = line.strip()
         if "." not in line:
             break
         sigma.append(list(line))
-       
-	    
-    except EOFError:
-        break
 
 x = 0
 y= 0 
@@ -35,21 +35,41 @@ for i in range(len(sigma)):
         if sigma[i][j] == "^":
             x = i
             y = j
-
 positions = set()
+obs = 0
+duplicate_found = False
 
 while True:
-    positions.add((x, y))
-    char = sigma[x][y]
-    new_x, new_y = direction(char, x, y)
-    if (new_x < 0 or new_x > len(sigma)-1 or new_y < 0 or new_y > len(sigma[0])-1):     
-           print(len(positions))
-           break
-    if sigma[new_x][new_y] == "#":
-        char = switchto(char)
-    else:
-         x, y = new_x, new_y
-    sigma[x][y] = char
+    for i in range(len(sigma)):
+        if duplicate_found:
+            break
+        for j in range(len(sigma[0])):
+            directions = []
+            if sigma[x][y] == sigma[i][j]:
+                continue
+            else:
+                sigma[i][j] = "#"
+            
+            char = sigma[x][y]
+            new_x, new_y = direction(char, x, y)
+            if (new_x < 0 or new_x > len(sigma)-1 or new_y < 0 or new_y > len(sigma[0])-1):     
+                break
+            
+            if sigma[new_x][new_y] == "#":
+                char = switchto(char)
+            else:
+                x, y = new_x, new_y
+                directions.append(x+y)
+                sigma[x][y] = char
+            
+            positions.add((x, y))
+            
+            if len(directions) != len(set(directions)):
+                obs += 1
+                duplicate_found = True
+                break
+    
+    if duplicate_found:
+        break
 
 
-print(len(positions))
